f1f377b1d5b6235bf919c1fdc3f78854a50a9e84
amenibelhaj <blameni073@gmail.com>
2025-12-10 22:11:00 +0100

    shapes

--- math/linear_algebra/2-main.py
+++ math/linear_algebra/2-main.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
+"""
+2-size_me_please
+Module containing the function matrix_shape(matrix)
+that calculates the shape (dimensions) of a nested list matrix.
+"""
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
+def matrix_shape(matrix):
+    """
+    Calculates the shape (dimensions) of a matrix represented as a nested list.
+    Args:
+        matrix (list): The matrix whose shape is to be determined.
+                       Assumed to be uniform (all elements have the same shape).
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
+    shape = []
+    current_level = matrix
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+    while isinstance(current_level, list):
+        shape.append(len(current_level))
+        
+        if len(current_level) == 0:
+            break
+        else:
+            current_level = current_level[0]
+
+    return shape
