d30df8b57186c716df05de786cc04b81f04bf82f
amenibelhaj <blameni073@gmail.com>
2025-12-10 22:16:52 +0100

    shapes

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,14 +3,15 @@
 Function to calculate the shape (dimensions) of a matrix.
 """
 
+
 def matrix_shape(matrix):
     """
     Calculates the shape (dimensions) of a matrix.
 
     Args:
         matrix (list): The matrix whose shape is to be determined.
-                       Assumed to be uniform (all elements have the same shape).
-    
+        Assumed to be uniform (all elements have the same shape).
+
     Returns:
         list: A list of integers representing the shape of the matrix.
     """
@@ -19,7 +20,7 @@ def matrix_shape(matrix):
 
     while isinstance(current_level, list):
         shape.append(len(current_level))
-        
+
         if len(current_level) == 0:
             break
         else:
