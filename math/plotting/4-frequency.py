c2f6e5a4838c0f89965fc369b9aa9d622a8c1142
amenibelhaj <blameni073@gmail.com>
2025-12-14 21:38:31 +0100

    histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,8 +4,6 @@ Module that plots a histogram of student grades.
 """
 
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')  # Ensure non-interactive backend for grading
 import matplotlib.pyplot as plt
 
 
@@ -16,11 +14,13 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
+    # Create figure
     plt.figure(figsize=(6.4, 4.8))
+
+    # Plot histogram: bins every 10 units, black edges
     plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+
+    # Label axes and title
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    
-    plt.savefig("plot.png")  # saves the figure for the grader
-    plt.show()  # optional for you to see the plot locally
