12b93f48037391b7429c7be6442a6318207b7d82
amenibelhaj <blameni073@gmail.com>
2025-12-21 20:53:05 +0100

    tree depth

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Module for building a Decision Tree
+"""
+import numpy as np
+
+
+class Node:
+    """
+    Represents an internal node in a decision tree
+    """
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """
+        Initializes the node
+        """
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """
+        Recursively calculates the maximum depth of the tree below this node
+        """
+        # We ask both children for their max depth
+        # and return the highest value found.
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+
+class Leaf(Node):
+    """
+    Represents a leaf node in a decision tree
+    """
+    def __init__(self, value, depth=None):
+        """
+        Initializes the leaf
+        """
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """
+        Returns the depth of the leaf itself
+        """
+        return self.depth
+
+
+class Decision_Tree():
+    """
+    Represents a decision tree structure
+    """
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """
+        Initializes the decision tree
+        """
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """
+        Calculates the maximum depth of the entire tree
+        """
+        return self.root.max_depth_below()
