903db03e085ba4bc9b55b718149254158786791d
amenibelhaj <blameni073@gmail.com>
2026-02-02 14:11:54 +0100

    neuron

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python3
+"""
+Wrote a class Neuron that defines a single neuron performing binary
+classification
+"""
+
 import numpy as np
 
 
 class Neuron:
-    """Single neuron with private attributes for binary classification"""
-
+    """
+    Class that defines a single neuron performing binary classification
+    """
     def __init__(self, nx):
-        if type(nx) is not int:
+        """
+        Constructor for the neuron class.
+        """
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be positive")
+            raise ValueError("nx must be a positive integer")
+
+        # Weight vector initialized using a random normal distribution
         self.__W = np.random.randn(1, nx)
+        # Bias initialized to 0.
         self.__b = 0
+        # Activation output initialized to 0.
         self.__A = 0
 
     @property
     def W(self):
+        """
+        Getter for the private attribute __W.
+        """
         return self.__W
 
     @property
     def b(self):
+        """
+        Getter for the private attribute __b.
+        """
         return self.__b
 
     @property
     def A(self):
+        """
+        Getter for the private attribute __A
+        """
         return self.__A
