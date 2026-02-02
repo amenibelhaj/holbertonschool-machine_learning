b251c8cd8624613056ff36cddac4ef6b2abb68fc
amenibelhaj <blameni073@gmail.com>
2026-02-02 16:46:18 +0100

    neuron

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+This script defines a Deep Neural Network for binary classification.
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Define a deep neural network that does binary classification.
+    """
+    def __init__(self, nx, layers):
+        """
+        Initialize a deep neural network with given number of input features
+        and layers.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or not layers:
+            raise TypeError("layers must be a list of positive integers")
+
+        # Check if all layers are positive integers
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)  # number of layers
+        self.cache = {}  # to store all intermediary values of the network
+        self.weights = {}  # to hold all weights and biases of the network
+
+        # Initialize weights and biases using He et al. method 4 each layer
+        for layer_index in range(1, self.L + 1):
+            layer_size = layers[layer_index - 1]
+            prev_layer_size = nx if layer_index == 1 else layers[
+                    layer_index - 2
+                    ]
+            self.weights[f'W{layer_index}'] = (
+                    np.random.randn(layer_size, prev_layer_size) * np.sqrt(
+                        2 / prev_layer_size
+                        )
+                    )
+            self.weights[f'b{layer_index}'] = np.zeros((layer_size, 1))
