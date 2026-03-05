9a086a462e4145f8000d5d373a0220329387646d
amenibelhaj <blameni073@gmail.com>
2026-03-05 23:19:24 +0100

    sequential

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,46 +1,37 @@
 #!/usr/bin/env python3
-
+"""Module to build a Keras model"""
 import tensorflow.keras as K
+
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network with the Keras library.
-    
+
     nx: number of input features
     layers: list of nodes per layer
     activations: list of activation functions per layer
     lambtha: L2 regularization parameter
     keep_prob: probability a node will be kept (Dropout)
     """
-    # 1. Create the conveyor belt (the model)
     model = K.Sequential()
-    
-    # 2. Define the 'Tax' (L2 Regularization)
-    l2_reg = K.regularizers.l2(lambtha)
-    
-    # 3. Build each 'Station' (Layer)
+    reg = K.regularizers.l2(lambtha)
+
     for i in range(len(layers)):
         if i == 0:
-            # The very first layer needs to know the input size (nx)
             model.add(K.layers.Dense(
-                layers[i], 
-                activation=activations[i], 
-                kernel_regularizer=l2_reg, 
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=reg,
                 input_shape=(nx,)
             ))
         else:
-            # Subsequent layers figure out their input size automatically
             model.add(K.layers.Dense(
-                layers[i], 
-                activation=activations[i], 
-                kernel_regularizer=l2_reg
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=reg
             ))
-        
-        # 4. Add 'Nap Time' (Dropout) after hidden layers
-        # We don't add dropout after the final output layer (the last one)
+
         if i < len(layers) - 1:
-            # Keras Dropout uses 'rate' (how many to drop)
-            # rate = 1 - keep_probability
             model.add(K.layers.Dropout(1 - keep_prob))
-            
+
     return model
