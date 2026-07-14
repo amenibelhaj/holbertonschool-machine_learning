527ae4d544534a634d7f397bfc2b7861781df4cf
amenibelhaj <blameni073@gmail.com>
2026-07-14 14:37:11 +0200

    fixes

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -5,58 +5,68 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Performs forward propagation over a convolutional layer of a neural
-    network
+    Performs forward propagation over a convolutional layer
 
-    A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
-        the output of the previous layer
-    W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
-        kernels for the convolution
-    b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
-        applied to the convolution
-    activation: activation function applied to the convolution
-    padding: string that is either 'same' or 'valid', indicating the type
-        of padding used
-    stride: tuple of (sh, sw) containing the strides for the convolution
+    Args:
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
+        b: numpy.ndarray of shape (1, 1, 1, c_new)
+        activation: activation function
+        padding: "same" or "valid"
+        stride: tuple (sh, sw)
 
-    Returns: the output of the convolutional layer
+    Returns:
+        The output of the convolutional layer
     """
+
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
+    kh, kw, _, c_new = W.shape
     sh, sw = stride
 
+    # Calculate padding
     if padding == "same":
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
+        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
+        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
     elif padding == "valid":
-        ph, pw = 0, 0
-    else:
-        ph, pw = padding
+        ph = 0
+        pw = 0
 
-    A_prev_pad = np.pad(
+    # Add padding to input
+    A_pad = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode="constant",
-        constant_values=0,
+        mode="constant"
     )
 
-    h_new = int((h_prev + 2 * ph - kh) / sh) + 1
-    w_new = int((w_prev + 2 * pw - kw) / sw) + 1
+    # Output dimensions
+    h_new = int(((h_prev + 2 * ph - kh) / sh) + 1)
+    w_new = int(((w_prev + 2 * pw - kw) / sw) + 1)
 
     Z = np.zeros((m, h_new, w_new, c_new))
 
+    # Convolution operation
     for i in range(h_new):
         for j in range(w_new):
-            v_start = i * sh
-            v_end = v_start + kh
-            h_start = j * sw
-            h_end = h_start + kw
-            A_slice = A_prev_pad[:, v_start:v_end, h_start:h_end, :]
             for k in range(c_new):
+                vert_start = i * sh
+                vert_end = vert_start + kh
+
+                horiz_start = j * sw
+                horiz_end = horiz_start + kw
+
+                a_slice = A_pad[
+                    :,
+                    vert_start:vert_end,
+                    horiz_start:horiz_end,
+                    :
+                ]
+
                 Z[:, i, j, k] = np.sum(
-                    A_slice * W[:, :, :, k], axis=(1, 2, 3)
+                    a_slice * W[:, :, :, k],
+                    axis=(1, 2, 3)
                 )
 
+    # Add bias and activation
     Z = Z + b
     A = activation(Z)
 
