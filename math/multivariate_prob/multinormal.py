8095df6162f9b9d95ca21c902fda9c80c9056e58
amenibelhaj <blameni073@gmail.com>
2026-09-22 13:06:05 +0200

    multivariate probability: task 3 pdf

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -26,3 +26,30 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         centered = data - self.mean
         self.cov = (centered @ centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        Calculate the PDF at a data point.
+
+        Args:
+            x (numpy.ndarray): shape (d, 1), the data point whose PDF
+                should be calculated
+
+        Returns:
+            float: the value of the PDF
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+        if x.ndim != 2 or x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        diff = x - self.mean
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        coefficient = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+        exponent = -0.5 * (diff.T @ inv @ diff)
+
+        return float(coefficient * np.exp(exponent[0][0]))
