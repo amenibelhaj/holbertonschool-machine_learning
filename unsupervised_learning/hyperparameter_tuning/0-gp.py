abeb98092cfaf070c7bdd743858a362587e528c3
amenibelhaj <blameni073@gmail.com>
2026-09-22 22:53:35 +0200

    hyperparameter tuning: task 0 fix line break style

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -38,7 +38,7 @@ class GaussianProcess:
         Returns:
             numpy.ndarray: shape (m, n), the covariance kernel matrix
         """
-        sq_dist = (np.sum(X1 ** 2, axis=1).reshape(-1, 1)
-                   + np.sum(X2 ** 2, axis=1) - 2 * X1 @ X2.T)
+        sq_dist = (np.sum(X1 ** 2, axis=1).reshape(-1, 1) +
+                   np.sum(X2 ** 2, axis=1) - 2 * X1 @ X2.T)
 
         return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sq_dist)
